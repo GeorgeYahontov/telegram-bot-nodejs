235e3e75dbc61c36c485b0a7afb8bf62dee9aeed
George <yaxontov1995@gmail.com>
2024-03-20 23:08:43 +0200

    watching console body message

--- src/common/logging/logging.middleware.ts
+++ src/common/logging/logging.middleware.ts
@@ -1,10 +1,12 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import {NextFunction} from "express";
+import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggingMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log(`Incoming Request: ${req.method} ${req.url} ${req.body}`);
+    // Для лучшей читаемости можно использовать JSON.stringify с параметрами
+    const requestBody = JSON.stringify(req.body, null, 2);
+    console.log(`Incoming Request: ${req.method} ${req.url} Body: ${requestBody}`);
     next();
   }
 }
