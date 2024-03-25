c1ecd26f6e570e910ea7c878d62f337e0cbed0dd
George <yaxontov1995@gmail.com>
2024-03-25 20:53:40 +0200

    Little corrections in middleware

--- src/common/logging/logging.middleware.ts
+++ src/common/logging/logging.middleware.ts
@@ -2,6 +2,7 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
+    //LoggingMiddleware
 export class LoggingMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const requestBody = JSON.stringify(req.body, null, 2);
